79bf48cd636dd8a3cb9c9e8a41f40b8aa87e5ec6
hfangjian <357987103@qq.com>
2017-06-30 18:04:29 +0800

    初始化项目

---
+++ src/main/java/Tutorial.java
@@ -0,0 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hfangjian on 2017/6/30.
+ */
+public class Tutorial {
+
+    private static final transient Logger log = LoggerFactory.getLogger(Tutorial.class);
+
+    public static void main(String[] args) {
+        log.info("My First Apache Shiro Application");
+        System.exit(0);
+    }
+
+}
---
+++ src/main/java/com.ctg.auth/Tutorial.java
@@ -0,0 +1,19 @@
+package com.ctg.auth;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hfangjian on 2017/6/30.
+ */
+public class Tutorial {
+
+    private static final transient Logger log = LoggerFactory.getLogger(Tutorial.class);
+
+    public static void main(String[] args) {
+        log.info("My First Apache Shiro Application");
+        System.exit(0);
+    }
+
+}
